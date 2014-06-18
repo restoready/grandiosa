9a0d2f35ca9c72d1772bd2047b4b1672c7ace8e4
Kévin Bizien <bizienkevin@gmail.com>
2014-06-18 18:25:25 +0200

    beginning integration page "carte"

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-	$('.menu-small__btn').click(function(){
+	$('.nav-small__btn').click(function(){
+        $('.sb-slidebar').show;
+        $('.sb-slidebar').animate({width:'toggle'},600);
+    });
+
+    $('.nav-small__btn2').click(function(){
         $('.sb-slidebar').show;
         $('.sb-slidebar').animate({width:'toggle'},600);
     });
